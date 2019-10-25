03e07944fd8df402994f93f0aed60817abf4a62f
yuqinggen <15622329294@163.com>
2019-10-25 18:42:41 +0800

    Reproduce phenomenon，always receive roll back event

--- src/test/java/com/example/testso58552671/TestSo58552671ApplicationTests.java
+++ src/test/java/com/example/testso58552671/TestSo58552671ApplicationTests.java
@@ -5,8 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationEventPublisher;
-import org.springframework.transaction.support.TransactionCallback;
-import org.springframework.transaction.support.TransactionTemplate;
 
 @SpringBootTest
 class TestSo58552671ApplicationTests {
@@ -14,8 +12,8 @@ class TestSo58552671ApplicationTests {
 	@Autowired
 	private ApplicationEventPublisher publisher;
 
-	@Autowired
-	private TransactionTemplate transactionTemplate;
+//	@Autowired
+//	private TransactionTemplate transactionTemplate;
 
 	@Test
 	public void noTransaction() throws InterruptedException {
@@ -23,13 +21,13 @@ class TestSo58552671ApplicationTests {
 		Thread.sleep(1000);
 	}
 
-	@Test
-	public void rollbackTransaction() throws InterruptedException {
-		transactionTemplate.execute((Void) -> {
-			publisher.publishEvent(new TransactionEvent("test"));
-			throw new IllegalStateException("Rollback!");
-		});
-		Thread.sleep(1000);
-	}
+//	@Test
+//	public void rollbackTransaction() throws InterruptedException {
+//		transactionTemplate.execute((Void) -> {
+//			publisher.publishEvent(new TransactionEvent("test"));
+//			throw new IllegalStateException("Rollback!");
+//		});
+//		Thread.sleep(1000);
+//	}
 
 }
